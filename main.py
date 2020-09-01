ae8d911903bb96044bbcbe4d2f7aa7273589958b
Martin Civan <martin.civan5@gmail.com>
2020-09-02 00:43:33 +0200

    uasyncio http request to get date from server

--- main.py
+++ main.py
@@ -1,49 +1,48 @@
-from machine import Pin
 import machine
-from time import sleep
-from machine import Pin, I2C
-import ssd1306
+from machine import Pin#, I2C
+# import ssd1306
 from network import WLAN
 import uasyncio as asyncio
 import uaiohttpclient as aiohttp
 
+PIN_SENZOR = 33
+PIN_NAPAJANIE_SENZOR = 27
+
 KONSTANTA = 2000
-POCET_MERANI = 5
+SEKUND_SUCHA = 60 * 60 * 24 * 2
+MERANIE_POCET = 5
+MERANIE_PAUZA = 1
 
-napajanie_senzor = machine.Pin(27, machine.Pin.OUT)
-vstup = machine.ADC(Pin(33))
 
-
-def zapni():
+async def zmeraj():
+    napajanie_senzor = machine.Pin(PIN_NAPAJANIE_SENZOR, machine.Pin.OUT)
     napajanie_senzor.value(True)
-
-
-def zmeraj():
-    sleep(1)
-    return vstup.read()
-
-
-def vypni():
+    vstup = machine.ADC(Pin(PIN_SENZOR))
+    hodnoty = []
+    for i in range(MERANIE_POCET):
+        await asyncio.sleep(MERANIE_PAUZA)
+        hodnoty.append(vstup.read())
     napajanie_senzor.value(False)
+    return round(sum(hodnoty) / len(hodnoty))
 
 
-def cvrk(sec=2, d=1000, freq=500):
+async def cvrk(sec=2, d=1000, freq=500):
     pin = machine.PWM(Pin(14), freq)
     pin.duty(d)
-    sleep(sec)
+    await asyncio.sleep(sec)
     pin.duty(0)
 
 
-def nachystaj_displej():
-    i2c = I2C(-1, scl=Pin(22), sda=Pin(21))
-    oled_width = 128
-    oled_height = 64
-    return ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-
-
-oled = nachystaj_displej()
-oled.text("Zapnuty", 0, 0)
-oled.show()
+# def nachystaj_displej():
+#     i2c = I2C(-1, scl=Pin(22), sda=Pin(21))
+#     oled_width = 128
+#     oled_height = 64
+#     return ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+#
+#
+# oled = nachystaj_displej()
+# oled.text("Zapnuty", 0, 0)
+# oled.show()
 
 wlan = WLAN()
 wlan.active(True)
@@ -51,59 +50,39 @@ if wlan.active():
     wlan.connect("SSID", "heslo")
     while not wlan.isconnected():
         machine.idle()  # save power while waiting
-    oled.text("Pripojeny", 0, 0)
-    oled.text(wlan.ifconfig()[0], 0, 8)
-    oled.show()
+    # oled.text("Pripojeny", 0, 0)
+    # oled.text(wlan.ifconfig()[0], 0, 8)
+    # oled.show()
     # response = urequests.get('http://pumec.zapto.org/')
     # oled.text(response.text, 0, 16)
-# else:
-#     oled.text("Wifi nejde :(", 0, 0)
-# oled.show()
-
-def nainstaluj():
-    import upip
-    upip.install('micropython-uasyncio')
-    upip.install('micropython-uasyncio.synchro')
-    upip.install('micropython-uasyncio.queues')
-
-    oled.text("Stiahnute", 0, 8)
-    oled.show()
 
 
-def print_stream(resp):
-    print((yield from resp.read()))
-    return
-    while True:
-        line = yield from reader.readline()
-        if not line:
-            break
-        print(line.rstrip())
+async def run(method, url, data=None):
+    resp, writer = await aiohttp.request(method=method, url=url, data=data, timeout=30)
+    text = await resp.read()
+    writer.aclose()
+    text = text.decode("ascii")
+    resp.text = text
+    return resp
 
+async def getDateTime():
+    response = await run(method="POST", url="http://192.168.1.26:8080/api/v1/1oC2DlvMktSdNZLnmhKZ/telemetry", data="{\"iny_nazov_kluca\": 54}")
+    datestring = response.headers["Date"]
+    print("Mam datum: " + datestring)
+    # date = datetime.strptime(datestring, " %a, %d %b %Y %H:%M:%S %Z")
+    # print("naparsovane")
+    # print(str(date.hour))
+    return datestring
 
-def run(url):
-    resp = yield from aiohttp.request("POST", url, data="{\"iny_nazov_kluca\": 54}", timeout=30)
-    print(resp)
-    yield from print_stream(resp)
+async def ohlassa():
+    datum = await getDateTime()
 
-
+asyncio.set_debug(True)
 loop = asyncio.get_event_loop()
-loop.run_until_complete(run("http://pumec.zapto.org:8080/api/v1/9Pt11blnxGVHNHETiop9/telemetry"))
+loop.run_until_complete(ohlassa())
 loop.close()
-#
-sleep(5)
-r = 0
-while True:
-    oled.text("Odmeral: " + str(zmeraj()), 0, r*8)
-    oled.show()
-    r = r + 1 if r < 4 else 0
-    if r==0:
-        oled.fill(0)
-sleep(1)
-zapni()
-namerane = sum([zmeraj() for i in range(POCET_MERANI)]) / POCET_MERANI
-vypni()
-print("namerane: " + str(namerane))
-if namerane > KONSTANTA:
-    cvrk()
+
+# if namerane > KONSTANTA:
+#     cvrk()
 # machine.deepsleep(6 * 60 * 60 * 1000)
-machine.deepsleep(10 * 1000)
+# machine.deepsleep(10 * 1000)
--- uaiohttpclient.py
+++ uaiohttpclient.py
@@ -6,8 +6,10 @@ class ClientResponse:
     def __init__(self, reader):
         self.content = reader
 
-    def read(self, sz=-1):
-        return (yield from self.content.read(sz))
+    async def read(self, sz=-1):
+        result = await self.content.read(sz)
+        self.content.aclose()
+        return result
 
     def __repr__(self):
         # return "<ClientResponse %d %s>" % (self.status, self.headers)
@@ -20,20 +22,21 @@ class ChunkedClientResponse(ClientResponse):
         self.content = reader
         self.chunk_size = 0
 
-    def read(self, sz=4*1024*1024):
+    async def read(self, sz=4*1024*1024):
         if self.chunk_size == 0:
-            l = yield from self.content.readline()
+            l = await self.content.readline()
             l = l.split(b";", 1)[0]
             self.chunk_size = int(l, 16)
             if self.chunk_size == 0:
                 # End of message
-                sep = yield from self.content.read(2)
+                sep = await self.content.read(2)
                 assert sep == b"\r\n"
+                self.content.aclose()
                 return b''
-        data = yield from self.content.read(min(sz, self.chunk_size))
+        data = await self.content.read(min(sz, self.chunk_size))
         self.chunk_size -= len(data)
         if self.chunk_size == 0:
-            sep = yield from self.content.read(2)
+            sep = await self.content.read(2)
             assert sep == b"\r\n"
         return data
 
@@ -41,7 +44,7 @@ class ChunkedClientResponse(ClientResponse):
         return "<ChunkedClientResponse %d %s>" % (self.status, self.headers)
 
 
-def request_raw(method, url, data=None, timeout=None):
+async def request_raw(method, url, data=None, timeout=None):
     try:
         proto, dummy, host, path = url.split("/", 3)
     except ValueError:
@@ -54,37 +57,40 @@ def request_raw(method, url, data=None, timeout=None):
         port = int(port)
     except ValueError:
         port = 80
-    if timeout:
-        reader, writer = yield from asyncio.wait_for(asyncio.open_connection(host, port), timeout)
-    else:
-        reader, writer = yield from asyncio.run(asyncio.open_connection(host, port))
+    # reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout)
+    reader, writer = await asyncio.open_connection(host, port)
+    # reader, writer = await asyncio.run(asyncio.open_connection(host, port))
     # Use protocol 1.0, because 1.1 always allows to use chunked transfer-encoding
     # But explicitly set Connection: close, even though this should be default for 1.0,
     # because some servers misbehave w/o it.
     content_length = ''
     if data is not None:
         content_length = "content-length: %i\r\n" % (len(data))
-    query = "%s /%s HTTP/1.0\r\nHost: %s:%i\r\n%sConnection: close\r\nUser-Agent: compat\r\n\r\n" % (method, path, host, port, content_length)
-    yield from writer.awrite(query.encode('latin-1'))
-    if data:
-        yield from writer.awrite(data)
-
-    return reader
+    query = "%s /%s HTTP/1.0\r\nHost: %s:%i\r\n%sConnection: close\r\nUser-Agent: compat\r\n\r\n%s" % (method, path, host, port, content_length, data)
+    await writer.awrite(query.encode('latin-1'))
+    return reader, writer
 
 
-def request(method, url, data=None, timeout=0):
+async def request(method, url, data=None, timeout=0):
     redir_cnt = 0
     redir_url = None
+    chunked = False
     while redir_cnt < 2:
-        reader = yield from request_raw(method, url, data, timeout)
+        (reader, writer) = await request_raw(method, url, data, timeout)
         headers = []
-        status = yield from reader.readline()
-        # sline = yield from reader.readline()
-        # protover, status, msg = sline.split(None, 2)
-        # status = int(status)
-        chunked = False
+        sline = await reader.readline()
+        data = sline.split(None, 2)
+        protocol = data[0]
+        try:
+            status = int(data[1])
+        except ValueError:
+            status = 0
+        try:
+            message = data[2]
+        except IndexError:
+            message = ""
         while True:
-            line = yield from reader.readline()
+            line = await reader.readline()
             if not line or line == b"\r\n":
                 break
             headers.append(line)
@@ -94,10 +100,10 @@ def request(method, url, data=None, timeout=0):
             elif line.startswith(b"Location:"):
                 url = line.rstrip().split(None, 1)[1].decode("latin-1")
 
-        # if 301 <= status <= 303:
-        #     redir_cnt += 1
-        #     yield from reader.aclose()
-        #     continue
+        if 301 <= status <= 303:
+            redir_cnt += 1
+            await reader.aclose()
+            continue
         break
 
     if chunked:
@@ -105,5 +111,10 @@ def request(method, url, data=None, timeout=0):
     else:
         resp = ClientResponse(reader)
     resp.status = status
-    resp.headers = headers
-    return resp
+    resp.headers = {}
+    for i in map(lambda x: x.decode("ascii").rstrip(), headers):
+        split = i.split(":", 1)
+        resp.headers[split[0]] = split[1]
+    resp.message = message
+    resp.protocol = protocol
+    return resp, writer
